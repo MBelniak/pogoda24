ca6a7171592533a910d47c72d0e360997dcb57f9
MBelniak <rubik40s@wp.pl>
2020-02-24 18:56:20 +0100

    initial commit

---
+++ src/test/java/com/rubik/pogoda24/Pogoda24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubik.pogoda24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pogoda24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
