26bbd1ef0e54aefce3b75e22e22f086ea07dc15a
MBelniak <rubik40s@wp.pl>
2020-06-09 23:51:14 +0200

    Configure app for deployment - works OK

--- backend/src/main/java/com/rubik/backend/firestore/FirestoreDatabase.java
+++ backend/src/main/java/com/rubik/backend/firestore/FirestoreDatabase.java
@@ -6,8 +6,10 @@ import com.google.cloud.firestore.FirestoreOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Component
 public class FirestoreDatabase {
@@ -17,7 +19,7 @@ public class FirestoreDatabase {
         FirestoreOptions firestoreOptions =
                 FirestoreOptions.getDefaultInstance()
                         .toBuilder()
-                        .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("MyFirstProject-b915277d3604.json")))
+                        .setCredentials(ServiceAccountCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("MyFirstProject-b915277d3604.json")))
                         .setProjectId("orbital-voyage-235912")
                         .build();
         return firestoreOptions.getService();
