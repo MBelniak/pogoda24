cba7a625eef826b07c9892fe2404de6a6e5f066f
MBelniak <rubik40s@wp.pl>
2020-06-29 20:33:29 +0200

    Remove unused component

--- shared/src/main/shared24/src/index.tsx
+++ shared/src/main/shared24/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Copyright } from './Copyright';
 import { LoadingIndicator } from './LoadingIndicator';
 import { showModal, closeModal } from './ModalWindow';
-import { Navigation } from './Navigation';
 import { PagingBar } from './PagingBar';
 import { TopImage } from './TopImage';
 const styles = require('./sass/main.scss');
@@ -12,7 +11,6 @@ export {
     LoadingIndicator,
     closeModal,
     showModal,
-    Navigation,
     PagingBar,
     TopImage
 };
